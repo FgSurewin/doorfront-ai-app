08c2a4a3bdf0268edee4d0df930d520a09f4144c
tort8678 <102439231+tort8678@users.noreply.github.com>
2023-06-09 12:39:19 -0400

    google cloud run

--- server/database/index.ts
+++ server/database/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const localLink = "mongodb://127.0.0.1/crsp_docker_dev";
+//const localLink = "mongodb+srv://general:WtAdX3vjz05t5NAS@cluster0.bpaee.mongodb.net/";
 
 export const databaseLink =
   process.env.NODE_ENV === "production" ? process.env.WEB_LINK : localLink;
