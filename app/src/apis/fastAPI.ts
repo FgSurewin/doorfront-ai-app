c7f544c0f5cdd402088dbd314c2f1b598b9f2581
DianaAleksieieva <aleksodiana@gmail.com>
2025-08-05 16:48:38 -0400

    bug fix

--- app/src/apis/fastAPI.ts
+++ app/src/apis/fastAPI.ts
@@ -10,7 +10,7 @@ export interface UpdateResponse {
 }
 
 
-const BASE_URL = process.env.FAST_API_URL || "https://doorfront-fast-api-748982393343.europe-west1.run.app";
+const BASE_URL = process.env.FAST_API_URL || "https://doorfront-fastapi-server-748982393343.us-central1.run.app";
 
 export async function updateLabelCoordinates(imageId: string): Promise<UpdateResponse> {
   const response = await fetch(`${BASE_URL}/add-exactCoordinates-and-address`, {
